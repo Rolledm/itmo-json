dde120d447ddda84df8dafdff03705f3c0006bd2
Vladislav Migachev <rolledm@gmail.com>
2022-03-03 03:10:45 +0300

    https

--- app.js
+++ app.js
@@ -1,11 +1,19 @@
 const express = require('express')
-const app = express(process.env.PORT)
+const fs = require("fs")
+const https = require("https")
+
+const port = process.env.PORT ? process.env.PORT : 1613
+
+const crt = {
+    key: fs.readFileSync("./ssl/server.key"),
+    cert: fs.readFileSync("./ssl/server.crt")
+}
+
+const app = express()
 
 app.get("/", (req, res) => {
     console.log(req.headers.connection)
     res.send("Hello")
 });
 
-app.listen(80, () => {
-    console.log("hey");
-});
+https.createServer(crt, app).listen(port, () => {console.log("running on " + port)})
