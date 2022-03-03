81bb3f22c07470797ecbf43c32d57a1554a19a26
Vladislav Migachev <rolledm@gmail.com>
2022-03-03 03:56:02 +0300

    Simple server

--- app.js
+++ app.js
@@ -1,13 +1,6 @@
 const express = require('express')
-const fs = require("fs")
-const https = require("https")
 
 const port = process.env.PORT ? process.env.PORT : 1613
-
-const crt = {
-    key: fs.readFileSync("./ssl/server.key"),
-    cert: fs.readFileSync("./ssl/server.crt")
-}
 
 const app = express()
 
@@ -16,4 +9,11 @@ app.get("/", (req, res) => {
     res.send("Hello")
 });
 
-https.createServer(crt, app).listen(port, () => {console.log("running on " + port)})
+app.get("/result4/", (req, res) => {
+    console.log(req.headers.connection)
+    res.send({
+        "message": "itmo309574"
+    })
+});
+
+app.listen(port, () => {console.log(port)})
