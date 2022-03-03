e1685d37f175169956d2feadd3334edfa726e3ae
Vladislav Migachev <rolledm@gmail.com>
2022-03-03 16:22:57 +0300

    lab6

--- app.js
+++ app.js
@@ -1,24 +1,27 @@
-import cors from 'cors'
-
 export function appSrc(express, bodyParser, createReadStream, crypto, http) {
     const app = express()
-    app.use(cors({
-        origin: "*",
-        methods: "GET,POST,PUT,DELETE,OPTIONS",
-    }))
     
     app.use(bodyParser.urlencoded({extended: true}))
     
     app.get("/login/", (req, res) => {
+        console.log(req)
+        res.header('Access-Control-Allow-Origin', '*');
+        res.header('Access-Control-Allow-Methods', 'GET,POST,PUT,PATCH,OPTIONS,DELETE');
         res.send("itmo309574")
     });
 
     app.get("/code/", (req, res) => {
+        console.log(req)
+        res.header('Access-Control-Allow-Origin', '*');
+        res.header('Access-Control-Allow-Methods', 'GET,POST,PUT,PATCH,OPTIONS,DELETE');
         let str = createReadStream(import.meta.url.substring(7))
         str.on("open", () => {str.pipe(res)})
     });
 
     app.get("/sha1/:*", (req, res) => {
+        console.log(req)
+        res.header('Access-Control-Allow-Origin', '*');
+        res.header('Access-Control-Allow-Methods', 'GET,POST,PUT,PATCH,OPTIONS,DELETE');
         let str = req.params['0']
         let hash = crypto.createHash("sha1")
         hash.update(str)
@@ -26,20 +29,34 @@ export function appSrc(express, bodyParser, createReadStream, crypto, http) {
     });
 
     app.get("/req/", (req, res) => {
+        res.header('Access-Control-Allow-Origin', '*');
+        res.header('Access-Control-Allow-Methods', 'GET,POST,PUT,PATCH,OPTIONS,DELETE');
         let query = req.query["addr"]
-        http.get(query, (r) => {
-            r.pipe(res)
-        })
+        if (query) {
+            http.get(query, (r) => {
+                r.pipe(res)
+            })
+        } else {
+            res.send("itmo309574")
+        }
     });
 
     app.post("/req/", (req, res) => {
+        res.header('Access-Control-Allow-Origin', '*');
+        res.header('Access-Control-Allow-Methods', 'GET,POST,PUT,PATCH,OPTIONS,DELETE');
         let query = req.body["addr"]
-        http.get(query, (r) => {
-            r.pipe(res)
-        })
+        if (query) {
+            http.get(query, (r) => {
+                r.pipe(res)
+            })
+        } else {
+            res.send("itmo309574")
+        }
     });
 
     app.all("*", (req, res) => {
+        res.header('Access-Control-Allow-Origin', '*');
+        res.header('Access-Control-Allow-Methods', 'GET,POST,PUT,PATCH,OPTIONS,DELETE');
         res.send("itmo309574")
     })
 
