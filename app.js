772b2df6370e9499e5adb7a9d5a9d6f6b70b32a7
Vladislav Migachev <rolledm@gmail.com>
2022-03-03 05:12:23 +0300

    parse body and headers

--- app.js
+++ app.js
@@ -1,18 +1,31 @@
 const express = require('express')
+const cors = require('cors')
 
 const port = process.env.PORT ? process.env.PORT : 1613
 
 const app = express()
+
+app.use(cors({
+    origin: "*",
+    methods: "GET,POST,PUT,DELETE,OPTIONS",
+    headers: "x-test,Content-Type,Accept,Access-Control-Allow-Headers"
+}))
+
+app.use(express.text({type: "*/*"}))
 
 app.get("/", (req, res) => {
     console.log(req.headers.connection)
     res.send("Hello")
 });
 
-app.get("/result4/", (req, res) => {
-    console.log(req.headers.connection)
+app.post("/result4/", (req, res) => {
+    console.log(req)
+    console.log(req.body)
+
     res.send({
-        "message": "itmo309574"
+        "message": "itmo309574",
+        "x-result": req.headers["x-test"],
+        "x-body": req.body
     })
 });
 
