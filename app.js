c33588fcdae1208c6b08a0a487eb4b86ba291dbe
Vladislav Migachev <rolledm@gmail.com>
2022-03-05 01:23:56 +0300

    what

--- app.js
+++ app.js
@@ -1,63 +1,57 @@
 export default function(express, bodyParser, createReadStream, crypto, http) {
     const app = express();
-    const cors = {
+    const CORS = {
         'Access-Control-Allow-Origin': '*',
         'Access-Control-Allow-Methods': 'GET,POST,PUT,PATCH,OPTIONS,DELETE',
         'Access-Control-Allow-Headers': 'Content-Type, Accept, Access-Control-Allow-Headers',
         'Content-Type': 'text/plain; charset=utf-8'
-    }
-    
-    app.use(bodyParser.urlencoded({extended: true}));
-    
-    app.all("/login/", (req, res) => {
-        res.set(cors)
-        res.send("itmo309574")
-    });
+    };
+    const login = 'itmo309574';
+    app
+        .use(bodyParser.urlencoded({extend: true}))
+        .all('/login/', (req, res) => {
+            res.set(CORS);
+            res.send(login);
+        })
 
-    app.all("/code/", (req, res) => {
-        res.set(cors)
-        let str = createReadStream(import.meta.url.substring(7))
-        str.on("open", () => {str.pipe(res)})
-    });
-
-    app.all("/sha1/:input/", (req, res) => {
-        res.set(cors)
-        let hash = crypto.createHash("sha1")
-        hash.update(req.params.input)
-        res.send(hash.digest("hex"))
-    });
-
-    app.use(bodyParser.json());
-  
-    app.all("/req/", (req, res) => {
-        res.set(cors)
-        let query = ""
-        if (req.method == "GET") {
-            query = req.query.addr
-        } else if (req.method == "POST") {
-            query = req.body.addy 
-        } else {
-            res.send("itmo309574")
-        }
-        if (query != "") {
-            http.get(query, (r) => {
-                let rawData = '';
-                r.on('data', (chunk) => {
-                    rawData += chunk;
-                });
-                r.on('end', () => {
-                    res.send(rawData);
-                });
-            })
-        } else {
-            res.send("itmo309574")
-        }
-    });
-
-    app.all("*", (req, res) => {
-        res.set(cors)
-        res.send("itmo309574")
-    })
+        .all('/code/', (req, res) => {
+            res.set(CORS);
+            const path = import.meta.url.substring(7);
+            createReadStream(path).pipe(res);
+        })
+        .all('/sha1/:input/', (req, res) => {
+            res.set(CORS);
+            const hash = crypto.createHash('sha1')
+            .update(req.params.input)
+            .digest('hex')
+            res.send(hash);
+        })
+        .use(bodyParser.json())
+        .all('/req/', (req, res) => {
+            res.set(CORS);
+            if (req.method === "GET" || req.method === "POST") {
+                const url = req.method === "GET" ? req.query.addr : req.body.addr;
+                if (url) {
+                    http.get(url, (response) => {
+                        let rawData = '';
+                        response.on('data', (chunk) => {
+                            rawData += chunk;
+                        });
+                        response.on('end', () => {
+                            res.send(rawData);
+                        });
+                    });
+                } else {
+                    res.send(login);
+                }
+            } else {
+                res.send(login);
+            }
+        })
+        .all('/*', (req, res) => {
+            res.set(CORS);
+            res.send(login);
+        });
 
     return app;
 }
